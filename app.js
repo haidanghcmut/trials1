ff076e3804ead6948c063d5dcde70436e05acf2f
haidang <dang.vohaidang@hcmut.edu.vn>
2023-06-23 11:15:53 +0700

    feature1-1

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var btnQuery = document.getElementById("btnQuery");
 var btnAdd = document.getElementById("btnAdd");
+if (btnQuery) {
+  btnQuery.addEventListener("click", function () {
+    window.location.href = "/query";
+  });
+}
+const btnDelete = document.getElementById("btnDelete");
