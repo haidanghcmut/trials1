6c8ac802824812f7ec52149de51dc63675905e80
haidang <dang.vohaidang@hcmut.edu.vn>
2023-06-26 10:24:41 +0700

    feature2-2

--- feature2.js
+++ feature2.js
@@ -1,3 +1,3 @@
 const btnUpdate = document.querySelector("#update");
 const btnDelete = document.querySelector("#delete");
-const btnQuery = document.querySelector("#query");
+const btnQuery = document.querySelector("#query"); 
